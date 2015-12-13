ad5a5f8b6ce7a19147c8bf2a14bbacb03a8371cd
Adam J. Forster <adam@adamforster.org>
2015-12-13 11:30:59 +0000

    Add day 4

---
+++ 2015/day4/part2.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+import hashlib
+
+secret = 'iwrupvqb'
+
+index = 1
+while True:
+    digest = hashlib.md5('{}{}'.format(secret, index)).hexdigest()
+    if digest.startswith('000000'):
+        print '{} - {}'.format(index, digest)
+        break
+    index += 1
