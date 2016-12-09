16857fc168e7f1269025b979cc852032f80da761
Adam J. Forster <adamf@bullionbypost.co.uk>
2016-12-09 16:36:53 +0000

    Add 2016 day 3 part one

---
+++ 2016/day3/part1.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+
+def is_possible(*args):
+    (a, b, c) = map(int, *args)
+    return (a + b > c) and (b + c > a) and (c + a > b)
+
+
+with open('input.txt', 'r') as f:
+    count = 0
+    
+    print len([True for line in f if is_possible(line.split())])
